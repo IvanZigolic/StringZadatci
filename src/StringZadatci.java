3478732c14defb50c03e7fcc88ddc0344a2986f1
IvanZigolic <ivan.zigolic02@gmail.com>
2026-05-13 18:31:39 +0200

    V1 Zadatak brojanja znakova

---
+++ src/StringZadatci.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class StringZadatci {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Unesite tekst: ");
+        String unos = sc.nextLine();
+        System.out.println(unos);
+        unos = unos.trim();
+        int brs = 0;
+        int brn = 0;
+        int brz = 0;
+        for(char i : unos.toCharArray()) {
+        if(i>= 'a' && i<='z' || i>= 'A' && i<='Z'){
+            brs++;
+        }
+        else if(i>= '0' && i<= '9'){
+            brn++;
+        }
+        else if(i != ' '){
+            brz++;
+        }
+        }
+        System.out.println("Unos ima " + brs + " slova");
+        System.out.println("Unos ima " + brn + " brojeva");
+        System.out.println("Unos ima " + brz + " znakova");
+    }
+}
